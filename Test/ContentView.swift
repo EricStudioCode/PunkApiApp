15bf644285b2a3d796f7bbc5a168fa504cf491b1
Eric <ericaxt04@gmail.com>
2023-05-17 16:08:46 +0200

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Eric  on 17.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
